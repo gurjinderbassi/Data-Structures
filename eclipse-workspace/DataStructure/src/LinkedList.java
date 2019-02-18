c87bd2fd44d62c5ac19fc6e29831d1b971a2055e
Gurjinder <gurjinderkaur95@gmail.com>
2019-02-18 22:18:26 +0530

    Initial commit

---
+++ eclipse-workspace/DataStructure/src/LinkedList.java
@@ -0,0 +1,16 @@
+
+public class LinkedList {
+	static Node head = null;
+	static class Node{
+		int data;
+		Node next;
+		public Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+public static void main(String arg[]){
+	
+}
+
+}
---
+++ eclipse-workspace/DataStructure/src/Stacks.java
@@ -0,0 +1,7 @@
+
+public class Stacks {
+
+public static void main(String arg[]) {
+	System.out.println("Hello");
+}
+}
