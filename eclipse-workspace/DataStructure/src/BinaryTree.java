39cc8d6f378a1d87090fcc86b6b4085793dbfcda
Gurjinder <gurjinderkaur95@gmail.com>
2019-02-19 10:58:55 +0530

    Trees added

---
+++ eclipse-workspace/DataStructure/src/BinaryTree.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+public class BinaryTree {
+static Node root = null;
+Scanner scanner = new Scanner(System.in);
+BinaryTree tree = new BinaryTree();
+	static class Node{
+	int data;
+	Node lc,rc;
+	public Node(int data) {
+		this.data = data;
+		lc = rc= null;
+		
+	}
+}
+	public static void main(String arg[]) {
+		
+	}
+}
